0b0a4282d40ac5eda1cb9106a0e7a39ef425579f
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-06-17 18:58:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boat Control
+
+## Description
+
+Boat state with sound: fishing / panic / break
+
+## Technology
+
+- JS Library: [React](https://react.dev/)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="layout">
+        <h1>Boat Control</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
